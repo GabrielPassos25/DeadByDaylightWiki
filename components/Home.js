94278ca70d350cd61e1516c9b748161f70b82f4c
Gabriel Urano <gabrielurano30@gmail.com>
2021-05-17 17:56:16 -0300

    Remodelando

--- components/Home.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import {Image, View,Text, TouchableOpacity, StyleSheet} from 'react-native';
-const Home = () =>{
-    return(
-        <View style ={{flex:1, flexDirection:'row',padding:10, justifyContent:'center', marginTop:10}}></View>
-    )
-}
-
-export default Home;
